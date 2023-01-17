d6472df156e15c76826903b9dfe8cc0954d8a515
Ramzi Akremi <ramzi.dev@icloud.com>
2023-01-17 12:26:21 +1100

    build: add tailwindui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['@heroicons/vue'],
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
